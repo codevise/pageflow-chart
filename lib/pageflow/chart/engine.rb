11922654971e956b877a15bbf0fe635589e31100
Tim Fischbach <tfischbach@codevise.de>
2023-09-29 09:35:51 +0200

    Fix Zeitwerk compatibility
    Add paths to both `autoload_paths` and
    `eager_load_paths`. `eager_load_paths` is required to have paths
    covered by `zeitwerk:check`.
    
    Ignore and require Paperclip processors and configuration, which are
    cached during initialization and, thus, cannot be reloaded.
    
    For Rails < 6, we cannot add `lib` to the `eager_load_paths` since
    Resque eager loads [1], which then evaluates the engine class a second
    time. This time `autoload_paths` is already frozen, resulting in an
    error.
    
    REDMINE-19438
    
    [1] https://github.com/resque/resque/blob/v1.27.4/lib/resque/tasks.rb#L45

--- lib/pageflow/chart/engine.rb
+++ lib/pageflow/chart/engine.rb
@@ -5,7 +5,23 @@ module Pageflow
     class Engine < Rails::Engine
       isolate_namespace Pageflow::Chart
 
-      config.paths.add('lib', eager_load: true)
+      if Rails.respond_to?(:autoloaders)
+        lib = root.join('lib')
+
+        config.autoload_paths << lib
+        config.eager_load_paths << lib
+
+        initializer 'pageflow_chart.autoloading' do
+          Rails.autoloaders.main.ignore(
+            lib.join('generators'),
+            lib.join('pageflow/chart/configuration.rb'),
+            lib.join('pageflow/chart/version.rb')
+          )
+        end
+      else
+        config.autoload_paths << File.join(config.root, 'lib')
+      end
+
       config.assets.precompile += ['pageflow/chart/custom.css', 'pageflow/chart/transparent_background.css']
       config.i18n.load_path += Dir[config.root.join('config', 'locales', '**', '*.yml').to_s]
 
